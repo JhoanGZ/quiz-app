d93a0a8a5bb820c1ebe36c2a39712379281cc105
JhoanGZ <jgzapata79@gmail.com>
2026-03-03 13:13:04 -0300

    fix: CSP inline scripts, youtube.be frames, and video embed URLs

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,8 +4,8 @@ Rails.application.configure do
     policy.font_src    :self, :https, :data
     policy.img_src     :self, :https, :data
     policy.object_src  :none
-    policy.script_src  :self, :https
+    policy.script_src  :self, :https, :unsafe_inline
     policy.style_src   :self, :https, :unsafe_inline
-    policy.frame_src   :self, "https://www.youtube.com", "https://www.youtube-nocookie.com", "https://player.vimeo.com"
+    policy.frame_src   :self, "https://www.youtube.com", "https://www.youtube-nocookie.com", "https://youtu.be", "https://player.vimeo.com"
   end
 end
